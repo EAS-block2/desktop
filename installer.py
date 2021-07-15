d53452a4524042763426537cc5aed38bb344fe25
Jake Ellington <cannotilever@protonmail.com>
2021-07-15 10:25:15 -0400

    cleaner output from main.rs's terminal window

--- installer.py
+++
@@ -1,4 +0,0 @@
-import shutil
-shutil.copyfile(".\\desktop.exe","C:\\Program Files\\EAS\\desktop.exe")
-shutil.copyfile(".\\display.exe","C:\\Program Files\\EAS\\display.exe")
-shutil.copyfile(".\\desktop.lnk","C:\\Users\\Tech2\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\EAS.lnk")
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use reqwest;
-use std::{process::*, str, thread, time::Duration};
+use std::{process::*, str, thread, time::Duration, time::SystemTime};
 
 fn main() {
+    println!("==========================Welcome to the HVRHS Emergency Alert System==========================\n");
+    println!("_________________________________________________________________________________________________");
+    println!("* This Application listens for alarm activations and will notify you if they occur");
+    println!("* The type and timestamp of any alarm condition will be displayed here as well as a fullscreen popup");
+    println!("* Please leave this window minimized, closing it will disable alarm listening");
+    println!("* To control alarm conditions and view detailed information, navegate to http://easrvr/ in a browser");
     let mut active: bool;
     let mut lastactive = false;
     let mut lbody: String= String::new();
-    println!("user is: {}",get_user());
+    if cfg!(debug_assertions) {println!("user is: {}",get_user());}
     let url = format!("http://easrvr:8000/pc/{}", get_user());
     loop {
     let body: String; 
@@ -13,15 +19,18 @@ fn main() {
         Ok(out) => {body = out.text().unwrap();}
         Err(_) => {body = String::from("fault");}}
     match body.as_str(){
-        "unauthorized" => {println!("unauth error!");break;}
+        "unauthorized" => {println!("You, {}, are unauthorized! Program halt.",get_user());
+        loop{thread::sleep(Duration::from_secs(1));}}
         "clear" => active = false,
         "fault" => active = lastactive,
         _ => active = true,
     }
-    println!("active:{}, body: {}", &active, &body);
+    
+    if cfg!(debug_assertions) {println!("active:{}, body: {}", &active, &body);}
     thread::sleep(Duration::from_millis(200));
     if active && (!lastactive || (body != lbody)){
         lastactive = true;
+        println!("\n Alert! Alarm Condition {0} was activated at {1:?}!",&body, SystemTime::now());
         if cfg!(target_os = "windows"){Command::new("C:\\Program Files\\EAS\\display.exe")
         .arg(&body).spawn().unwrap();}
         else {Command::new("/usr/bin/python3")
